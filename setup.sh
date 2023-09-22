13dab9e3fb1ea20b407139607ed9b5b85dd8bfcc
Guy Menahem <guy@komodor.io>
2023-09-22 16:27:43 +0300

    install ArgoCD before the workshop starts

--- setup.sh
+++ setup.sh
@@ -18,6 +18,8 @@ echo "
 |gitHub CLI      |Yes                  |'https://cli.github.com/'                          |
 |yq              |Yes                  |'https://github.com/mikefarah/yq#install'          |
 |kubeseal        |Yes                  |'https://github.com/bitnami-labs/sealed-secrets#kubeseal'|
+|Helm            |Yes                  |'https://github.com/helm/helm#install'             |
+|kubectl         |Yes                  |'https://github.com/kubernetes/kubectl'            |
 " | gum format
 
 gum confirm "
@@ -25,7 +27,7 @@ Do you have those tools installed?
 " || exit 0
 
 GITHUB_ORG=$(gum input \
-    --placeholder "Which GitHub organization do you want to use?" \
+    --placeholder "Which GitHub organization do you want to use (usually your GH username)?" \
     --value "$GITHUB_ORG")
 echo "export GITHUB_ORG=$GITHUB_ORG" >> .env
 
@@ -97,3 +99,38 @@ echo "export BACKSTAGE_URL=$BACKSTAGE_URL" >> .env
 
 yq --inplace ".data.BASE_URL = \"$BACKSTAGE_URL\"" \
     backstage-resources/bs-config.yaml
+
+###########
+# Argo CD #
+###########
+
+helm upgrade --install argocd argo-cd \
+    --repo https://argoproj.github.io/argo-helm \
+    --namespace argocd --create-namespace \
+    --values argocd/helm-values.yaml --wait
+
+echo "http://argocd.$INGRESS_HOST.nip.io"
+
+# Open the URL from the output in a browser
+# Use `admin` as the user and `admin123` as the password
+
+cat argocd/production-apps.yaml
+
+cat argocd/production-infra.yaml
+
+kubectl apply --filename argocd/production-infra.yaml
+
+kubectl apply --filename argocd/production-apps.yaml
+
+export ARGOCD_ADMIN_PASSWORD=admin123
+
+argocd login --insecure --port-forward --insecure \
+    --username admin --password $ARGOCD_ADMIN_PASSWORD \
+    --port-forward-namespace argocd --grpc-web --plaintext
+
+# Generate API auth token for ArgoCD
+export ARGOCD_AUTH_TOKEN=$(argocd account generate-token \
+    --port-forward --port-forward-namespace argocd) >> .env
+
+export ARGOCD_AUTH_TOKEN_ENCODED=$(
+    echo -n "argocd.token=$ARGOCD_AUTH_TOKEN" | base64) >> .env
