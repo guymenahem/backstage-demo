45bb14eefddaa45fdd9dc6e6d335f87ff5bfa10e
Guy Menahem <guy@komodor.io>
2023-09-23 17:44:54 +0300

    fix argo secrets

--- demo.sh
+++ demo.sh
@@ -74,7 +74,7 @@ kubectl --namespace backstage \
     --from-literal POSTGRES_USER=app \
     --from-literal POSTGRES_PASSWORD=$DB_PASS \
     --from-literal GITHUB_TOKEN=$GITHUB_TOKEN \
-    --from-literal ARGOCD_AUTH_TOKEN=$ARGOCD_AUTH_TOKEN \
+    --from-literal ARGOCD_AUTH_TOKEN=$ARGOCD_AUTH_TOKEN_ENCODED \
     --dry-run=client --output json
 
 kubectl --namespace backstage \
@@ -82,7 +82,7 @@ kubectl --namespace backstage \
     --from-literal POSTGRES_USER=app \
     --from-literal POSTGRES_PASSWORD=$DB_PASS \
     --from-literal GITHUB_TOKEN=$GITHUB_TOKEN \
-    --from-literal ARGOCD_AUTH_TOKEN=$ARGOCD_AUTH_TOKEN \
+    --from-literal ARGOCD_AUTH_TOKEN=$ARGOCD_AUTH_TOKEN_ENCODED \
     --dry-run=client --output yaml \
     | kubeseal --controller-namespace kubeseal \
     | tee backstage-resources/bs-secret.json
